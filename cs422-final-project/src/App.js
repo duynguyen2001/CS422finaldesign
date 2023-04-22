726a134b1e9c55ba95f9003601c22da797dc977c
Khanh Duy Nguyen <55223995+duynguyen2001@users.noreply.github.com>
2023-04-21 20:30:03 -0500

    new commit

--- cs422-final-project/src/App.js
+++ cs422-final-project/src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 import Header from './components/Header';
 import { Router, Routes, Route } from 'react-router';
 import SearchPage from './components/SearchPage';
-
+import OptionComponent from './OptionComponent';
+import BillGatesImage from './components/Image/Bill_gates.jpg';
 function App() {
   return (
     <div className="App">
+      <OptionComponent name="Khanh" numberOfStars={4} imageLink={BillGatesImage} description="ahahahahahah" position="Header PC"/>
       <SearchPage/>
+
     </div>
   );
 }

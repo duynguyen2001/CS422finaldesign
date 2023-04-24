3b029ffc1c1770079d9f9ed43ad8b388463c43e1
Kshiteesh Kandari <kshiteeshkandari@gmail.com>
2023-04-23 19:20:49 -0500

    Made the three Pages

---
+++ cs422-final-project/src/components/Contact.js
@@ -0,0 +1,95 @@
+import styles from "./Contact.module.css";
+const Contact = ({ onClose }) => {
+  return (
+    <div className={styles.contact}>
+      <div
+        className={styles.metadata}
+      >{`{"config":{},"type":"Contact-Desktop","nodeName":"Contact - Desktop","children":["637c6948-0021-41cc-bc73-6a0484374565","e767fc0a-4135-4295-ae27-0e2de42a106e","8e45fab0-c1dc-4c57-83b3-5e8bebbe6558","167680e8-5fa7-46fb-9e0d-0ba315478b5c","0fadf912-b641-420a-acf7-0b9b301c993d","b9a5aee4-b3cd-4c2c-b6eb-80d5dba812f0","f3098009-a55c-4afd-b3b0-8a7948dbdf1d","7c859b36-6a02-4f1b-94f9-35ad90cb62b8","9a2c35e3-0717-410c-b69e-8e9b025d5c16","50c79164-e6fa-434d-af14-a7d56418c042","5c43befc-2d8f-48e0-a124-dde118877c63","cb058603-0fc5-4b16-9b6d-562bd9275baf","5a65d768-b0da-45f9-b03b-f61baa2b54e6","e46508d5-1d00-48bb-8eea-3e909c8ce1d2","2296405c-2132-4df0-a6a1-45281681cf3e","0cbc7697-f991-4714-a4f4-0df63711df28","ce9cf063-4e47-481e-9c7e-2693e6ca28ac","5da5eda6-f7da-49d0-a953-42aaeb06354f","7e0a3027-5d90-47a7-813a-128e6a647595","b83f7212-1894-4803-8f48-bc062ae2eb13","f945aeb0-d4a0-45a0-a994-c6424d2b9792","e299d0cc-21db-4209-8569-31903a0b63cd","6302dc92-0836-4f3a-aebc-b0ae759228f0","8dbc1b96-e7f4-4688-9371-7a4615956b4d","ae1c6831-36e1-43d7-95a1-b3bced207329","b9988e56-803c-41ef-87c3-95bd93eaf7f5","25b0b0f7-b7c3-427d-b89d-d6ab12caaaa3","39be968b-90e3-469e-83c9-b98ea1d94389","58dfddd5-92fd-4406-b89e-1c7015cee4b5","cae5ae17-bb7c-46b4-aa2b-a90bb97d2a49","26f880ac-50cb-42e4-aba1-398612e9aa5e","53cb1a6b-eb31-45c8-a79b-0dd94ed1401c","420e4d4e-6cc4-4ad6-a619-24f4332eb0f8","4f3a0e52-b9fd-4ab2-a1aa-c4e4169891d4","a406c185-9d54-4015-a72f-66bba7c6c804","c775c1bc-2a6a-4657-91f4-e11e62ba9fd3","82032786-e810-4754-b3de-cdb786bdaf38","b16d2546-c5da-47af-a4fa-16bf9cde4fd9","e9c94e9c-0ff1-4518-92ef-339b3d4dd1c2","d6ff6f85-1602-4156-8c67-f0a13ac21565","bb48b6af-190e-45ec-8908-e3e77da10a4c","d77d78f2-9593-483f-80e7-3a50d3d1312c","4d58c13b-9409-4974-b336-fa9cdd11eae7","17ea04a7-82e4-4c52-ba19-2f5e5bb934f2","6794b739-e76f-43e3-95e5-1add4f2f4139","b2beda29-3898-48aa-8a63-ac57e057c4fa","f852116a-368d-405b-90ea-253cadee5ec4","18860d95-05b3-4055-a9f8-781786a63974","07ddc537-228d-4464-a809-c3001a27d179","cb3fdbde-f198-4e43-ab24-c2d123a05646","ffdb87e5-5ae3-4ee6-b17b-3f67f5de1571","51f054e4-8f73-4a48-8052-86c7598b12d7","5274045c-77b6-45b1-a12a-2abc4b7d473f","0aa0f72d-4d58-45ab-8c7f-410f07c4799e","4219b3ed-99c3-437e-88eb-868a4a6ff565","d68cf2ad-0b2e-4fbc-a756-251d10936eb2","b5513424-b5d6-4c64-9860-37d57a939e30","5106bf12-72df-4ac9-b278-584fa11601ff","b7dae93d-16c3-4a9e-b8b2-b5df0643d8ce","898b3eee-e083-4bd3-9b1d-c1b3283a895c","6f3a3c65-eff9-4e59-bacb-178645fec5c5","267b9216-b531-4df1-928f-71ee74659229","acf034da-eb92-4a7a-a493-6be66c3ce0f5","6753e67a-490a-4188-8239-9b3ddb81ee67","cec44731-b0a9-4ab3-b2b0-2900a017d4f8","e3ea246d-b4ee-4f8e-8fd7-e95baec8a350","4a8c1bfb-55bb-4bb8-ba19-2b0228833e32","1bac0780-0f4a-490e-b036-7be6a2583f3d","f444f4de-aebe-4ddb-89b0-05593dd368dd","58a1d467-206d-40ca-9f3e-0af1859aa1f2","c94efce7-5336-4c72-a9b2-3ba768cd66e9","3eeab252-b89a-4176-a4f1-2651ffae4c53","97225e11-845c-4308-9b25-2a0019066163","294ea109-962f-44d2-8823-2da2b461a378","1e7bfa3e-c31e-4ed0-afc2-7973e4cc594e"],"__plugin":"Mockup","__version":"1.5.0","__lastUpdate":"2023-03-16T01:22:37.499Z"}`}</div>
+      <div className={styles.signUp}>
+        <div
+          className={styles.metadata1}
+        >{`{"config":{},"nodeName":"Sign Up","type":"Group","__plugin":"Mockup","__version":"1.5.0","__lastUpdate":"2023-03-16T01:22:37.493Z"}`}</div>
+        <div className={styles.cardDisplayElementsdefault}>
+          <div
+            className={styles.metadata2}
+          >{`{"config":{},"type":"Card","__plugin":"Mockup","__version":"1.5.0","__lastUpdate":"2023-03-16T01:22:37.285Z","nodeName":"Card [DISPLAY_ELEMENTS=DEFAULT]"}`}</div>
+        </div>
+        <img
+          className={styles.backgroundStylestyle2Icon}
+          alt=""
+          src="/background-stylestyle2@2x.png"
+        />
+        <div className={styles.locationPin}>
+          <div
+            className={styles.metadata3}
+          >{`{"config":{},"nodeName":"Location Pin","type":"Group","__plugin":"Mockup","__version":"1.5.0","__lastUpdate":"2023-03-16T01:22:37.293Z"}`}</div>
+          <img className={styles.ellipseIcon} alt="" src="/ellipse.svg" />
+          <img className={styles.ellipseIcon1} alt="" src="/ellipse1.svg" />
+        </div>
+        <button className={styles.buttonDisplayElementslabel}>
+          <div
+            className={styles.metadata4}
+          >{`{"config":{},"type":"Button","__plugin":"Mockup","__version":"1.5.0","__lastUpdate":"2023-03-16T01:22:37.345Z","nodeName":"Button [DISPLAY_ELEMENTS=Label][SIZE=MEDIUM][STATE=DEFAULT][STYLE=STYLE1]"}`}</div>
+          <div className={styles.label}>Send</div>
+          <div className={styles.iconDisplayElementslabel}>
+            <div
+              className={styles.metadata4}
+            >{`{"config":{"STATE":"DEFAULT"},"type":"Icon","__plugin":"Mockup","__version":"1.5.0","__lastUpdate":"2023-03-16T01:22:37.334Z","nodeName":"Icon [DISPLAY_ELEMENTS=Label][ICON=feather/heart][SIZE=MEDIUM][STATE=DEFAULT][STYLE=STYLE1]"}`}</div>
+            <img className={styles.icon} alt="" src="/icon.svg" />
+          </div>
+        </button>
+        <input
+          className={styles.inputDisplayElementsdefaul}
+          type="text"
+          placeholder="Email Address"
+        />
+        <input
+          className={styles.inputDisplayElementsdefaul1}
+          type="text"
+          placeholder="Name"
+        />
+        <textarea
+          className={styles.textareaDisplayElementsdef}
+          placeholder="Write your message here"
+        />
+        <div className={styles.circleButtonSizemediumst}>
+          <div
+            className={styles.metadata}
+          >{`{"config":{"STYLE":"STYLE2"},"type":"CircleButton","__plugin":"Mockup","__version":"1.5.0","__lastUpdate":"2023-03-16T01:22:37.447Z","nodeName":"Circle Button [SIZE=MEDIUM][STATE=DEFAULT][STYLE=STYLE2]"}`}</div>
+          <div className={styles.iconIconfeatherxsizemed}>
+            <div
+              className={styles.metadata}
+            >{`{"config":{"STATE":"DEFAULT","STYLE":"STYLE2","ICON":"feather/x"},"type":"Icon","__plugin":"Mockup","__version":"1.5.0","__lastUpdate":"2023-03-16T01:22:37.441Z","nodeName":"Icon [ICON=feather/x][SIZE=MEDIUM][STATE=DEFAULT][STYLE=STYLE2]"}`}</div>
+            <img className={styles.icon1} alt="" src="/icon1.svg" />
+          </div>
+        </div>
+        <div className={styles.iconIconfeathermapPinsi}>
+          <div
+            className={styles.metadata}
+          >{`{"config":{"SIZE":"LARGE","ICON":"feather/map-pin"},"__plugin":"Mockup","__version":"1.5.0","__lastUpdate":"2023-03-16T01:22:37.457Z","type":"Icon","nodeName":"Icon [ICON=feather/map-pin][SIZE=LARGE][STYLE=STYLE1]"}`}</div>
+          <img className={styles.icon2} alt="" src="/icon2.svg" />
+        </div>
+        <div className={styles.iconIconfeatherphonesize}>
+          <div
+            className={styles.metadata}
+          >{`{"config":{"SIZE":"LARGE","ICON":"feather/phone"},"__plugin":"Mockup","__version":"1.5.0","__lastUpdate":"2023-03-16T01:22:37.469Z","type":"Icon","nodeName":"Icon [ICON=feather/phone][SIZE=LARGE][STYLE=STYLE1]"}`}</div>
+          <img className={styles.icon3} alt="" src="/icon3.svg" />
+        </div>
+        <div className={styles.iconIconfeathersmartphone}>
+          <div
+            className={styles.metadata}
+          >{`{"config":{"SIZE":"LARGE","ICON":"feather/smartphone"},"__plugin":"Mockup","__version":"1.5.0","__lastUpdate":"2023-03-16T01:22:37.484Z","type":"Icon","nodeName":"Icon [ICON=feather/smartphone][SIZE=LARGE][STYLE=STYLE1]"}`}</div>
+          <div className={styles.icon4}>
+            <div className={styles.a423c526596248ea990941bfff} />
+          </div>
+        </div>
+        <div className={styles.address}>123 Mockup St., New York</div>
+        <div className={styles.phone}>{`(+1) 123 456 7890 `}</div>
+        <div className={styles.mobile}>(+1) 123 456 7891</div>
+        <b className={styles.titleTagh4}>Get In Touch</b>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;

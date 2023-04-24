9e0f89f5361d20554b80e336a1871403a7de9b00
Kshiteesh Kandari <kshiteeshkandari@gmail.com>
2023-04-23 21:01:03 -0500

    check this
    New pages

--- cs422-final-project/src/App.js
+++ cs422-final-project/src/App.js
@@ -1,32 +1,20 @@
 import './App.css';
 import { Router, Routes, Route } from 'react-router';
 import SearchPage from './components/SearchPage';
-<<<<<<< HEAD
-import OptionComponent from './OptionComponent';
-import BillGatesImage from './components/Image/Bill_gates.jpg';
-import ExpertPage from './components/ExpertPage';
-import PaymentPage from './components/PaymentPage';
-function App() {
-  return (
-    <div className="App">
-      {/* <OptionComponent name="Khanh" numberOfStars={4} imageLink={BillGatesImage} description="ahahahahahah" position="Header PC"/> */}
-      {/* <SearchPage/> */}
-      {/* <ExpertPage /> */}
-      <PaymentPage />
-
-=======
 import BookingPage from './components/BookingPage';
 import BookingHistory from './components/BookingHistory';
+import MessagePage from './pages/MessagePage';
+import FeedbackPage from './pages/FeedbackPage';
+import VideoCall from './pages/VideoCall';
 
 
 
 function App() {
   return (
     <div className="App">
-      <SearchPage/>
+      <FeedbackPage/>
       {/* <BookingPage /> */}
       {/* <BookingHistory /> */}
->>>>>>> 11f7ef2af4ffffb21a5bb2fcb2820efbd92b3f1f
     </div>
   );
 }
